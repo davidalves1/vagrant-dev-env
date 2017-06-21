a81da246c5814c5a6e412d788b89f3f39a86ee77
David Alves <dev.david@freterapido.com>
2017-06-21 13:56:01 -0300

    Alterações para instalação básica do apache

--- Vagrantfile
+++ Vagrantfile
@@ -13,14 +13,14 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 
   # Create a forwarded port mapping which allows access to a specific port
   # within the machine from a port on the host machine. 
-  config.vm.network "forwarded_port", guest: 80,    host: 8080 # http
-  config.vm.network "forwarded_port", guest: 3306,  host: 33006 # mysql
+  config.vm.network "forwarded_port", guest: 80,    host: 80 # http
+  config.vm.network "forwarded_port", guest: 3306,  host: 3306 # mysql
 
   # Create a private network, which allows host-only access to the machine
   config.vm.network "private_network", ip: "192.168.30.100"
 
   # Share an additional folder to the guest VM.
-  # config.vm.synced_folder "your/local/folder", "vm/respective/folder"
+  # Ex: config.vm.synced_folder "your/local/folder", "vm/respective/folder"
   config.vm.synced_folder 'html/', '/var/www/html/', nfs: true
   config.vm.synced_folder 'home/', '/home/vagrant/', nfs: true
 
@@ -30,7 +30,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
     vb.gui = false
 
     # set the vm name in virtualbox
-    vb.name = "vagrant-dev-env"
+    vb.name = "freterapido-dev"
 
     # Use VBoxManage to customize the VM. For example to change memory:
     vb.customize ["modifyvm", :id, "--memory",               "512"]
@@ -42,7 +42,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   # config.ssh.shell = "bash -c 'BASH_ENV=/etc/profile exec bash'"
 
   # install some base packages with Apache
-  # config.vm.provision :shell, privileged: false, path: "bootstrap-apache.sh"
+  config.vm.provision :shell, privileged: false, path: "bootstrap-apache.sh"
 
   # install some base packages with Nginx
   # config.vm.provision :shell, privileged: false, path: "bootstrap-nginx.sh"
--- bootstrap-apache.sh
+++ bootstrap-apache.sh
@@ -9,10 +9,6 @@ sudo add-apt-repository ppa:ondrej/php
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
-# Add Mysql 5.7 list file
-wget http://dev.mysql.com/get/mysql-apt-config_0.6.0-1_all.deb
-sudo dpkg -i mysql-apt-config_0.6.0-1_all.deb
-
 # Update apt-get
 # --------------------
 sudo apt-get update
@@ -20,41 +16,22 @@ sudo apt-get update
 echo -e "##### Install base packages #####"
 sudo apt-get install -y vim curl build-essential python-software-properties git
 
-# echo '##### Install Mysql #####'
+# # For now MySQL must be installed manually
+# # echo '##### Install Mysql #####'
 # # --------------------
-sudo apt-get install -y -f mysql-server
-sudo mysql_secure_installation
-sudo mysql_install_db
+# 
+# # Add Mysql 5.7 list file
+# wget http://dev.mysql.com/get/mysql-apt-config_0.6.0-1_all.deb
+# sudo dpkg -i mysql-apt-config_0.6.0-1_all.deb
+# 
+# sudo apt-get update
+# 
+# sudo apt-get install -y mysql-server
 
 echo '##### Install Apache #####'
 # --------------------
-sudo apt-get install -y -f apache2
+sudo apt-get install -y apache2
 sudo chmod -R 777 /var/www
-
-# Replace contents of default Apache vhost
-# --------------------
-VHOST=$(cat <<EOF
-NameVirtualHost *:8080
-Listen 8080
-<VirtualHost *:80>
-  DocumentRoot "/var/www/html"
-  ServerName localhost
-  <Directory "/var/www/html">
-    AllowOverride All
-  </Directory>
-</VirtualHost>
-<VirtualHost *:8080>
-  DocumentRoot "/var/www/html"
-  ServerName localhost
-  <Directory "/var/www/html">
-    AllowOverride All
-  </Directory>
-</VirtualHost>
-EOF
-)
-
-echo "$VHOST" > sudo /home/ubuntu/000-default.conf
-sudo mv /home/ubuntu/000-default.conf /etc/apache2/sites-enabled/000-default.conf
 
 sudo a2enmod rewrite
 
@@ -62,7 +39,7 @@ sudo service apache2 restart
 
 echo '##### Install PHP 7.0 or 5.6 #####'
 # --------------------
-sudo apt-get install -y -f php7.0 libapache2-mod-php7.0 php7.0-cli php7.0-common php7.0-mbstring php7.0-gd php7.0-intl php7.0-xml php7.0-mysql php7.0-mcrypt php7.0-zip
+sudo apt-get install -y php7.0 libapache2-mod-php7.0 php7.0-cli php7.0-common php7.0-mbstring php7.0-gd php7.0-intl php7.0-xml php7.0-mysql php7.0-mcrypt php7.0-zip
 # sudo apt-get install -y -f php5.6 libapache2-mod-php5.6 php5.6-cli php5.6-common php5.6-mbstring php5.6-gd php5.6-intl php5.6-xml php5.6-mysql php5.6-mcrypt php5.6-zip
 
 echo '##### Install Node #####'
@@ -74,7 +51,7 @@ curl -sL https://deb.nodesource.com/setup_7.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 
 sudo apt-get update
-sudo apt-get install -y -f nodejs
+sudo apt-get install -y nodejs
 
 node -v
 
